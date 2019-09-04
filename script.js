d9c0212bbf1c2c4cfdd8e3313e1a054cc5c28d1c
guram dgebuadze <guramidgebuadze@gmail.com>
2019-09-04 12:35:58 +0400

    changed menu and  img hover in key-feature

---
+++ script.js
@@ -0,0 +1,29 @@
+let img = document.querySelectorAll('.key-features--col-my');
+let  imgHover = document.querySelectorAll('.hover-text');
+
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener('mouseover', () => {
+        if (i%2 === 0) {
+            imgHover[i].classList.add('img-hov');
+        } else {
+            imgHover[i].classList.add('img-hov-left');
+        }
+    })
+
+    img[i].addEventListener('mouseout', () => {
+        imgHover[i].classList.remove('img-hov');
+        imgHover[i].classList.remove('img-hov-left');
+    })
+}
+
+let showNav = document.querySelector('.nav-div').style;
+
+document.querySelector('.header__button').addEventListener('click', () => {
+    setTimeout(() => {
+    if (showNav.display === 'block') {
+        showNav.display = 'none';
+    } else {
+        showNav.display = 'block';
+    } 
+}, 400); 
+});
